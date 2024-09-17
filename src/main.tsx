3b71fe9066686fab79f90b3b38686f62ba0dff7a
Flavius Catalin Neacsum <kete102@gmail.com>
2024-09-17 17:49:08 +0200

    feat(auth): clerk auth
    Added Clerk auth

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import { ClerkProvider } from '@clerk/clerk-react'
 import ErrorPage from './error-page.tsx'
 import { HomePage } from './routes/HomePage.tsx'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 const router = createBrowserRouter([
   {
@@ -15,6 +18,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
   </StrictMode>
 )
--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -4,7 +4,7 @@ export const HomePage = () => {
   return (
     <Container>
       <h1 className="text-5xl font-bold text-white">Kanban Board</h1>
-      <section></section>
+      <section className="justify-middle mt-10 flex h-[80dvh] w-full max-w-7xl flex-col rounded-md bg-black text-white"></section>
     </Container>
   )
 }
