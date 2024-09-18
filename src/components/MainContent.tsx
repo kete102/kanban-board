48375c46d2ba1f05b2174aa89ad3f38dced75ad7
Flavius Catalin Neacsum <kete102@gmail.com>
2024-09-18 12:54:20 +0200

    style(buttons): buttons
    create board and form buttons

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,4 +1,3 @@
-import { SignedIn, UserButton } from '@clerk/clerk-react'
 import React from 'react'
 
 interface Props {
@@ -9,26 +8,9 @@ interface Props {
 export const MainContent = ({ children, style }: Props) => {
   return (
     <section
-      className={`h-[80dvh] w-full max-w-screen-2xl rounded-md bg-white ${style}`}
+      className={`h-[80dvh] w-full max-w-screen-2xl rounded-md bg-zinc-900 ${style}`}
     >
-      <div className="flex h-full w-full flex-col rounded-md">
-        <section className="mx-4 my-5 p-4 text-right">
-          <SignedIn>
-            <UserButton
-              appearance={{
-                elements: {
-                  avatarBox: 'w-12 h-12'
-                },
-                variables: {
-                  fontSize: 'text-lg'
-                }
-              }}
-              showName={true}
-            />
-          </SignedIn>
-        </section>
-        {children}
-      </div>
+      {children}
     </section>
   )
 }
--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,7 +1,6 @@
-//TODO: Esta pagina es para cuando ya estas logedIn
 import { MainContent } from '@/components/MainContent'
+import { SignedIn, UserButton } from '@clerk/clerk-react'
 import {
-  Button,
   Description,
   Dialog,
   DialogPanel,
@@ -35,27 +34,37 @@ export const HomePage = () => {
 
   return (
     <MainContent style={isModalOpen ? 'blur-sm bg-white/95' : ''}>
-      <div className="h-full">
-        <button
-          onClick={handleOnClick}
-          className="flex items-center gap-2 rounded-lg border-2 border-black p-2 text-2xl font-bold"
-        >
-          New Board
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
+      <div className="flex h-full flex-col">
+        <section className="mx-4 flex items-center justify-between p-4 text-right">
+          <button
+            className="group relative inline-block"
+            onClick={handleOnClick}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            <span className="items-centrer relative z-10 flex justify-center overflow-hidden rounded-lg border-2 border-indigo-600 px-3.5 py-2 font-medium leading-tight text-indigo-600 transition-colors duration-300 ease-out group-hover:text-white">
+              <span className="absolute inset-0 h-full w-full rounded-lg bg-gray-50 px-5 py-3"></span>
+              <span className="ease absolute left-0 -ml-2 h-40 w-40 origin-top-right -translate-x-full translate-y-12 -rotate-90 bg-indigo-600 transition-all duration-300 group-hover:-rotate-180"></span>
+              <span className="relative text-lg font-semibold">New board</span>
+            </span>
+            <span
+              className="absolute bottom-0 right-0 -mb-1 -mr-1 h-9 w-full rounded-lg bg-indigo-600 transition-all duration-200 ease-linear group-hover:mb-0 group-hover:mr-0"
+              data-rounded="rounded-lg"
+            ></span>
+          </button>
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: 'w-12 h-12'
+                },
+                variables: {
+                  fontSize: 'text-lg',
+                  colorText: 'white'
+                }
+              }}
+              showName={true}
             />
-          </svg>
-        </button>
+          </SignedIn>
+        </section>
         <Dialog
           open={isModalOpen}
           as="div"
@@ -103,19 +112,21 @@ export const HomePage = () => {
                     </Field>
                   </Fieldset>
                   <div className="mt-4 inline-flex gap-4">
-                    <Button
-                      className="rounded-md bg-gray-700 px-4 py-1.5 text-lg/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
-                      type="submit"
+                    <button
+                      type="button"
+                      className="shadow-xs cursor-pointer rounded-lg bg-indigo-500 px-6 py-2.5 text-center text-lg font-semibold text-white transition-all duration-500 hover:bg-indigo-700"
                       onClick={handleAddNewBoard}
                     >
-                      Add
-                    </Button>
-                    <Button
-                      className="rounded-md bg-gray-700 px-4 py-1.5 text-lg/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
+                      Create
+                    </button>
+
+                    <button
+                      type="button"
+                      className="shadow-xs cursor-pointer rounded-lg bg-indigo-50 px-6 py-2.5 text-center text-lg font-semibold text-indigo-500 transition-all duration-500 hover:bg-indigo-100"
                       onClick={() => setIsModalOpen(false)}
                     >
                       Cancel
-                    </Button>
+                    </button>
                   </div>
                 </form>
               </DialogPanel>
