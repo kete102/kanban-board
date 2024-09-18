14c821267cfdd9fb82e052ac50838173b0f1f0f2
Flavius Catalin Neacsum <kete102@gmail.com>
2024-09-18 14:29:24 +0200

    fix(auth): fix routing
    Fixed routing on authStatus

--- src/routes/RootLayout.tsx
+++ src/routes/RootLayout.tsx
@@ -1,23 +1,25 @@
 import { Container } from '@/components'
 import { useAuth } from '@clerk/clerk-react'
 import { useEffect } from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 
 export const RootLayout = () => {
   const navigate = useNavigate()
+  const location = useLocation()
   const { isSignedIn } = useAuth()
 
   useEffect(() => {
-    if (!isSignedIn) {
+    if (!isSignedIn && location.pathname !== '/auth') {
+      // Si el usuario no esta auth y no esta en /auth, redirigir a /auth
       navigate('/auth')
-    } else {
-      navigate('/')
     }
-  }, [isSignedIn, navigate])
+  }, [isSignedIn, navigate, location.pathname])
 
   return (
     <Container>
-      <h1 className="mb-20 text-7xl font-bold text-white">Kanban Board</h1>
+      <h1 className="mb-20 mt-10 text-7xl font-bold text-white">
+        Kanban Board
+      </h1>
       {/* <MainContent> */}
       <Outlet />
       {/* </MainContent> */}
