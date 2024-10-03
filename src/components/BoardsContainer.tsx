a85f6aaeac3d77485801b66ef6e6503d522b138a
Neacsu Flavius Catalin <kete102@gmail.com>
2024-10-03 23:48:55 +0200

    style(no boards styles): no boards Styles
    no boards Styles

--- src/components/BoardsContainer.tsx
+++ src/components/BoardsContainer.tsx
@@ -40,16 +40,20 @@ export const BoardsContainer = ({ toggleOpenModal }: Props) => {
   return (
     <div
       id="boards-container"
-      className="flex h-auto min-h-fit w-fit items-center justify-center overflow-y-scroll rounded-md px-4 py-3 lg:max-w-full"
+      className={
+        boards.length !== 0
+          ? `flex h-auto min-h-fit w-fit items-center justify-center overflow-y-scroll rounded-md px-4 py-3 lg:max-w-full`
+          : 'grid h-full place-content-center'
+      }
     >
       {boards.length === 0 && (
-        <div className="grid h-fit w-fit place-content-center rounded-md bg-zinc-900 p-10 text-center">
+        <div className="grid h-full w-fit place-content-center rounded-md p-10 text-center">
           <section className="mb-6 w-full">
-            <p className="text-4xl text-zinc-300">Add your first board!</p>
+            <p className="text-4xl font-bold text-zinc-950">No boards yet!</p>
           </section>
           <section className="w-full">
             <button
-              className="inline-flex items-center justify-center gap-2 rounded-lg bg-indigo-600 px-5 py-2.5 text-2xl text-white shadow-lg shadow-indigo-800/50 transition-all duration-300 ease-in-out hover:scale-105 hover:bg-indigo-700 hover:shadow-xl active:scale-95"
+              className="inline-flex items-center justify-center gap-2 rounded-lg bg-indigo-600 px-5 py-2.5 text-2xl text-white shadow-lg shadow-indigo-800/50 transition-all duration-75 ease-in-out hover:scale-105 hover:bg-indigo-700 hover:shadow-xl active:scale-95"
               onClick={toggleOpenModal}
             >
               Add board
