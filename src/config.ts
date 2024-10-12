d146045602d79f77f5a1aa6d3a0212a46aab57e4
Neacsu Flavius Catalin <kete102@gmail.com>
2024-10-12 21:00:49 +0200

    fix(env tipo): env tipo
    env tipo

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-export const API_URL = import.meta.env.VITE_API_URl || 'http://localhost:3000'
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
