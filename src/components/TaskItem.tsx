3920eb87fcb2caab212a729c415c23d089ef1833
Neacsu Flavius Catalin <kete102@gmail.com>
2024-10-28 13:39:42 +0100

    style(taskitem.tsx): task styles
    task styles

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,5 +1,8 @@
 import Badge from '@/atom/Badge'
 import { Task } from '@/types'
+import clsx from 'clsx'
+import { BsCalendar2Check } from 'react-icons/bs'
+import { CiCalendar, CiEdit, CiTrash } from 'react-icons/ci'
 
 interface Props {
   task: Task
@@ -7,10 +10,48 @@ interface Props {
 
 export const TaskItem = ({ task }: Props) => {
   return (
-    <div className="w-full rounded-md bg-zinc-400/50 px-4 py-3" key={task._id}>
-      <h3 className="text-lg font-bold text-zinc-950">{task.taskTitle}</h3>
-      <p className="text-md mb-2 text-zinc-500">{task.taskDescription}</p>
-      <Badge priority={task.priority} />
+    <div
+      className="group w-full rounded-md bg-zinc-400/50 px-4 py-3"
+      key={task._id}
+    >
+      <section className="inline-flex w-full items-center justify-between">
+        <h3
+          className={clsx('text-lg font-bold text-zinc-950', {
+            'line-through': task.status === 'done'
+          })}
+        >
+          {task.taskTitle}
+        </h3>
+        <div className="flex items-center gap-3">
+          <CiEdit
+            size={23}
+            className="cursor-pointer lg:opacity-0 lg:group-hover:opacity-100"
+          />
+          <CiTrash
+            size={23}
+            className="cursor-pointer lg:opacity-0 lg:group-hover:opacity-100"
+          />
+        </div>
+      </section>
+      <p
+        className={clsx('text-md mb-2 text-zinc-500', {
+          'line-through': task.status === 'done'
+        })}
+      >
+        {task.taskDescription}
+      </p>
+      <section className="inline-flex w-full items-center justify-between">
+        <h5 className="inline-flex items-center gap-1 font-semibold text-zinc-500">
+          {task.status === 'done' ? (
+            <BsCalendar2Check size={20} />
+          ) : (
+            <CiCalendar size={24} />
+          )}
+          {new Date(task.endDate).toLocaleDateString()}
+        </h5>
+
+        <Badge priority={task.priority} />
+      </section>
     </div>
   )
 }
