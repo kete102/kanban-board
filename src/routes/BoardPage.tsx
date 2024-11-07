951b7fefa6277a5fec59d3e51a21300f98f1ebb1
Neacsu Flavius Catalin <kete102@gmail.com>
2024-11-07 17:20:19 +0100

    style(boardpage): boardPage navigation back button
    BoardPage navigation back button

--- src/routes/BoardPage.tsx
+++ src/routes/BoardPage.tsx
@@ -7,7 +7,7 @@ import useTaskStore from '@/store/TaskStore'
 import { DEFAULT_START_DATE } from '@/utils/dates'
 import { useAuth } from '@clerk/clerk-react'
 import { useEffect, useMemo, useState } from 'react'
-import { IoIosArrowRoundBack } from 'react-icons/io'
+import { IoIosArrowBack, IoIosArrowRoundBack } from 'react-icons/io'
 import { useNavigate, useParams } from 'react-router-dom'
 import { DateValueType } from 'react-tailwindcss-datepicker'
 
@@ -65,13 +65,16 @@ export const BoardPage = () => {
   return (
     <Container>
       <MainContent>
-        <section className="mx-auto mt-3 flex w-full max-w-lg flex-row items-center justify-between px-3 md:max-w-2xl lg:max-w-4xl xl:max-w-[1250px]">
-          <button onClick={handleNavigation}>
-            <IoIosArrowRoundBack size={35} />
+        <section className="mx-auto flex w-full max-w-lg flex-row items-center justify-between px-3 md:max-w-2xl lg:max-w-4xl xl:max-w-[1250px]">
+          <button
+            onClick={handleNavigation}
+            className="rounded-full border border-zinc-400/50 bg-zinc-300 p-2"
+          >
+            <IoIosArrowBack size={25} fill="#858483" />
           </button>
           <h1 className="text-md font-bold">Nombre de la board</h1>
         </section>
-        <div className="mx-auto mt-2 flex h-full w-full min-w-fit max-w-xs flex-col items-center justify-start gap-4 sm:max-w-md md:max-w-2xl lg:max-w-3xl lg:flex-row lg:items-start xl:max-w-6xl">
+        <div className="mx-auto mt-2 flex h-full max-h-fit w-full min-w-fit max-w-xs flex-col items-center justify-start gap-4 p-4 sm:max-w-md md:max-w-2xl lg:max-w-3xl lg:flex-row lg:items-start xl:max-w-6xl">
           {Array.from(columns).map(([columnType, column]) => (
             <KanbanColumn
               key={column.columnId}
