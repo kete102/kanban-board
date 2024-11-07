37508a72294f14344ba5c57eae9befd5dedf8dda
Neacsu Flavius Catalin <kete102@gmail.com>
2024-11-07 19:45:37 +0100

    chore(config): config
    config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,2 @@
 export const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-// export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
-export const API_URL =
-  'https://kanban-baord-app-backend-production.up.railway.app'
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
