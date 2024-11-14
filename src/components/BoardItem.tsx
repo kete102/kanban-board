ffa12137b041c0ed474140ec331a6ada9c4e6e48
Neacsu Flavius Catalin <kete102@gmail.com>
2024-11-14 12:34:01 +0100

    fix(boarditem): view button navigation fix
    View button navigation fix

--- src/components/BoardItem.tsx
+++ src/components/BoardItem.tsx
@@ -30,15 +30,15 @@ export const BoardItem = ({ board, onDelete }: Props) => {
         </h5>
       </div>
       <section className="flex gap-4">
-        <button className="inline-flex items-center rounded-md border-2 border-indigo-600 bg-indigo-300 px-5 py-2.5 text-sm font-bold text-zinc-600">
-          <Link
-            to={`/${userId}/boards/${board.boardId}`}
-            className="inline-flex items-center"
-          >
+        <Link
+          to={`/${userId}/boards/${board.boardId}`}
+          className="inline-flex items-center"
+        >
+          <button className="inline-flex items-center rounded-md border-2 border-indigo-600 bg-indigo-300 px-5 py-2.5 text-sm font-bold text-zinc-600">
             <BiSearch size={18} />
             View
-          </Link>
-        </button>
+          </button>
+        </Link>
         <button
           className="inline-flex items-center rounded-md border-2 border-red-700 bg-red-300 px-5 py-2.5 text-sm font-bold text-zinc-600"
           onClick={() => onDelete({ id: board.boardId })}
