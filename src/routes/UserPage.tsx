6c0bf7894d530b661f90c89bd4c00bcef163ebb1
Neacsu Flavius Catalin <kete102@gmail.com>
2024-11-14 12:35:35 +0100

    fix(userpage): get boards from store
    get boards from store

--- src/routes/UserPage.tsx
+++ src/routes/UserPage.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { BoardsContainer, Container, MainContent } from '@/components'
 import { useBoards } from '@/hooks/useBoards'
+import useBoardStore from '@/store/BoardStore'
 import { useEffect } from 'react'
 
 export const UserPage = () => {
-  const { getBoards, boards } = useBoards()
+  const { boards } = useBoardStore()
+  const { getBoards } = useBoards()
 
   useEffect(() => {
     if (boards.length === 0) {
